9b4b2ec1a6b61de0f54a452c44444aaaac8bb53d
Muhammad Jamal Khan <91218251+M-Jamal-K@users.noreply.github.com>
2023-10-01 01:11:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ import OutputCom from "./components/OutputCom";
 import { useFetch } from "./hooks/useFetch";
 
 function App() {
-  console.log(process.env.SEC_KEY)
-
   const { data } = useFetch(
     "https://json-server-rest-api.glitch.me/userData?id=true",
     "null"
@@ -31,6 +29,7 @@ function App() {
 
   return (
     <div className="App">
+<div>test ENV {process.env.SEC_KEY}</div>
       <Header />
       <InputCom />
       <OutputCom />
